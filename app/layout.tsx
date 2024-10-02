38c92a744ed7d9e5272a62e7ef8b857e3d6f48f3
Ajibaye Akeem <ajibayeakeem60@gmail.com>
2024-10-02 11:34:55 +0100

    implementation

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import { Inter as FontSans } from 'next/font/google';
+import '@/app/globals.css';
+import { cn } from '@/lib/utils';
+import { Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
+import Provider from './Provider';
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
+export const metadata: Metadata = {
+  title: 'LiveDocs',
+  description: 'Your collaborative editor',
+};
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        variables: {
+          colorPrimary: '#3371FF',
+          fontSize: '16px',
+        },
+      }}
+    >
+      <html lang='en' suppressHydrationWarning>
+        <body
+          className={cn(
+            'min-h-screen font-sans antialiased',
+            fontSans.variable
+          )}
+        >
+          <Provider>{children}</Provider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
